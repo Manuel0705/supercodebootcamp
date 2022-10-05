613e726cb64903fa2151d8540290aba0ee72fa98
Manuel0705 <Manuel-Petersen@gmx.net>
2022-10-05 18:40:56 +0200

    js einfuehrung

---
+++ 2022-05-10/js einführung variablen 1_5/main.js
@@ -0,0 +1,13 @@
+const carName = "BMW";
+console.log(carName);
+
+let x = 150;
+let y = 50;
+let z = x + y;
+console.log(z);
+
+// let person = firstName = "John" + lastName = "Doe" + age = 35;
+
+let [firstName,lastName,age] = ["John","Doe",35]
+console.log(firstName, lastName, age);
+
---
+++ 2022-05-10/js übung 1_7/main.js
@@ -0,0 +1,7 @@
+let helloWorld = "Hello World"
+const pi = 3.14
+const burjDubai = "828m"
+const fullName = "Jan Schmidt"
+const eifelTower = "324m"
+let camelcase = "camelCase"
+let kursstatus = "true"
