5d4e453e06b039ac1854cea6c925806edd9bc59b
Manuel0705 <Manuel-Petersen@gmx.net>
2022-10-06 18:59:32 +0200

    js 2

---
+++ 2022-06-10/js einführung 1_1 function/assets/JS/main.js
@@ -0,0 +1,30 @@
+// console.log("HALLO");
+
+
+
+// function intro() {
+//     let a = 1 + 3;
+//     console.log("hello world");
+//     console.log("1+3 = " + a);
+//     console.log("Hallo Manuel");
+// }
+
+// intro();
+
+
+
+// function intro2(paramName) {
+//     let varName = "SuperCoder";
+    
+//     console.log("Hi, " + varName + " Mein Name ist " + paramName + " . ");
+
+// }
+// intro2("Manuel");
+
+
+function intro3(paraName, paraStadt, paraAlter) {
+console.log("Hallo, mein Name ist " + paraName + ". Ich bin " + paraAlter + ". Ich komme aus " + paraStadt +".");
+}
+// intro3("SuperCoder","Duesseldorf",27);
+intro3("SuperCoder","Duesseldorf",27);
+
